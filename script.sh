7452fe72d5c2b1b6a5dc87f6a7f8180f2655d833
Ruta <gulbesvane@gmail.com>
2022-02-01 10:43:49 +0000

    first commit

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 3. reate 2 folders called fol_1, fol_2
+mkdir fol_1
+mkdir fol_2
+
+#go into fol_1
+cd fol_1
+# 4. create the following files in fol_1: 1_1.txt, 1_2.txt, 1_3.txt
+touch 1_1.txt
+touch 1_2.txt
+touch 1_3.txt
+
+#go back up one level
+cd ../
+#go into fol_2
+cd fol_2
+
+# 5. create the following files in fol_2: 2_1.txt, 2_2.txt, 2_3.txt
+touch 2_1.txt
+touch 2_2.txt
+touch 2_3.txt
+
